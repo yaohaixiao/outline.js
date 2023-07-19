47ab76b2824d2df7de1e780970c76c67173cbe25
Yaohaixiao <yaohaixiao@gmail.com>
2023-07-19 17:32:32 +0800

    feat: 生成独立模块：anchors.min.js、chapters.min.js、drawer.min.js 和 toolbar.min.js

--- build/rollup.config.lib.js
+++ build/rollup.config.lib.js
@@ -23,6 +23,47 @@ export default [
     plugins: PLUGIN_CONFIG
   },
   {
+    input: 'src/anchors.js',
+    output: {
+      name: 'Anchors',
+      file: './anchors.min.js',
+      format: 'umd',
+      sourcemap: true
+    },
+    plugins: PLUGIN_CONFIG
+  },
+  {
+    input: 'src/chapters.js',
+    output: {
+      name: 'Chapters',
+      file: './chapters.min.js',
+      format: 'umd',
+      sourcemap: true
+    },
+    plugins: PLUGIN_CONFIG
+  },
+  {
+    input: 'src/drawer.js',
+    output: {
+      name: 'Drawer',
+      file: './drawer.min.js',
+      format: 'umd',
+      sourcemap: true
+    },
+    plugins: PLUGIN_CONFIG
+  },
+  {
+    input: 'src/toolbar.js',
+    output: {
+      name: 'Toolbar',
+      file: './toolbar.min.js',
+      format: 'umd',
+      sourcemap: true
+    },
+    plugins: PLUGIN_CONFIG
+  },
+  // build for documentation
+  {
     input: 'src/outline.js',
     output: {
       name: 'Outline',
