d0864b6104275cecbefef576afa1102f091d97e1
Yaohaixiao <yaohaixiao@gmail.com>
2023-08-04 18:54:01 +0800

    chore: 调整 _getChapterParentIdByDiffer() 方法的变量名，使代码可读性更好

--- src/_getChapterParentIdByDiffer.js
+++ src/_getChapterParentIdByDiffer.js
@@ -1,14 +1,14 @@
 const _getChapterParentIdByDiffer = (chapters, differ, index) => {
-  let parent = chapters[index - 1]
+  let previous = chapters[index - 1]
   let pid
   let i
 
   for (i = 0; i < differ; i += 1) {
-    pid = parent.pid
-    parent = chapters[pid]
+    pid = previous.pid
+    previous = chapters[pid]
   }
 
-  pid = parent.pid
+  pid = previous.pid
 
   return pid
 }
