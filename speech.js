25c2195d7847919b7b27723e2d99d510bdb2b1df
Yaohaixiao <yaohaixiao@gmail.com>
2024-03-31 22:52:43 +0800

    chore: 完善 speech.js 模块

--- speech.js
+++ speech.js
@@ -1,45 +1,44 @@
-import isString from './utils/types/isString'
-import hasOwn from './utils/lang/hasOwn'
-import isObject from './utils/types/isObject'
-import extend from './utils/lang/extend'
+import publish from './utils/observer/emit'
 
 class Speech {
   constructor(options) {
-    this._utterance = new SpeechSynthesisUtterance()
+    this.utterance = new SpeechSynthesisUtterance()
 
     if (options) {
       this.initialize(options)
     }
   }
 
-  initialize(options) {
-    this.utterance(options)
+  _initialize(options) {
+    if (options.lang) {
+      this.setLang(options.lang)
+    }
+
+    if (options.pitch) {
+      this.setPitch(options.pitch)
+    }
+
+    if (options.rate) {
+      this.setRate(options.rate)
+    }
+
+    if (options.text) {
+      this.setText(options.text)
+    }
+
+    if (options.voice) {
+      this.setVoice(options.voice)
+    }
+
+    if (options.volume) {
+      this.setVolume(options.volume)
+    }
+
     return this
   }
 
-  utterance(prop, value) {
-    const attrs = this._utterance
-
-    if (isString(prop)) {
-      // 只能扩展 attrs 中已有的属性
-      if (value && hasOwn(attrs, prop)) {
-        // 更新单个配置信息
-        attrs[prop] = value
-        return this
-      }
-
-      // 只传递 prop 参数，则返回对应的属性值
-      return attrs[prop]
-    } else if (isObject(prop)) {
-      // 批量更新配置信息
-      extend(attrs, prop)
-
-      return this
-    } else if (arguments.length === 0) {
-      // 不传递参数，直接返回整个
-      return attrs
-    }
-
+  initialize(options) {
+    this._initialize(options)._addListeners()
     return this
   }
 
@@ -59,12 +58,42 @@ class Speech {
     return speechSynthesis.getVoices()
   }
 
+  setLang(lang) {
+    this.utterance.lang = lang
+    return this
+  }
+
+  setPitch(pitch) {
+    this.utterance.pitch = pitch
+    return this
+  }
+
+  setRate(rate) {
+    this.utterance.rate = rate
+    return this
+  }
+
+  setText(text) {
+    this.utterance.text = text
+    return this
+  }
+
+  setVoice(voice) {
+    this.utterance.voice = voice
+    return this
+  }
+
+  setVolume(volume) {
+    this.utterance.volume = volume
+    return this
+  }
+
   speak(text) {
     if (text) {
-      this.utterance('text', text)
+      this.setText(text)
     }
 
-    speechSynthesis.speak(this._utterance)
+    speechSynthesis.speak(this.utterance)
 
     return this
   }
@@ -81,7 +110,39 @@ class Speech {
 
   cancel() {
     speechSynthesis.cancel()
-    this.utterance('text', '')
+    this.setText('')
+    return this
+  }
+
+  _addListeners() {
+    const EVENTS = [
+      'boundary',
+      'end',
+      'error',
+      'start',
+      'mark',
+      'pause',
+      'resume'
+    ]
+    const utterance = this.utterance
+    const speech = this
+
+    EVENTS.forEach((name) => {
+      utterance[`on${name}`] = (event) => {
+        publish(name, {
+          event,
+          speech
+        })
+      }
+    })
+
+    speechSynthesis.onvoiceschanged = function (event) {
+      publish('voiceschanged', {
+        event,
+        speech
+      })
+    }
+
     return this
   }
 }
